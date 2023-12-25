acec78a9888af06bd460e5e5c278f5401282769b
jananpatel2002 <jkpatel2001@student.stcc.edu>
2023-12-24 19:10:20 -0500

    resetted readme

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,23 @@
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
 const SetupPage = async () => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/server/${server.id}`);
+  }
+  
   return <div>Create A Server</div>;
 };
 
